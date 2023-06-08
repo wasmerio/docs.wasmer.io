851725309f389023939548a67cceaf53fa6054b6
Syrus Akbary <me@syrusakbary.com>
2023-06-08 15:59:35 +0200

    Added new docs

--- next.config.js
+++ next.config.js
@@ -7,5 +7,41 @@ module.exports = {
   ...withNextra(),
   images: {
     unoptimized: true,
-  }
+  },
+  async redirects() {
+    return [
+      // Runtime
+      {
+        source: '/ecosystem/wasmer',
+        destination: '/runtime',
+        permanent: true,
+      },
+      {
+        source: '/ecosystem/wasmer/getting-started',
+        destination: '/runtime/get-started',
+        permanent: true,
+      },
+      {
+        source: '/ecosystem/wasmer/usage',
+        destination: '/runtime/cli',
+        permanent: true,
+      },
+      {
+        source: '/ecosystem/wasmer/wasmer-features',
+        destination: '/runtime/features',
+        permanent: true,
+      },
+      {
+        source: '/ecosystem/wasmer/building-from-source',
+        destination: '/runtime/developers/build-from-source',
+        permanent: true,
+      },
+      {
+        source: '/ecosystem/wasmer/building-from-source',
+        destination: '/runtime/developers/testing',
+        permanent: true,
+      },
+
+    ];
+  },
 }
