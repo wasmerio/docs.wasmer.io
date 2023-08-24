d86a17020cb3eda6c16f8ccfe06eebfe307899a7
Rudra Arora <aroraru@tcd.ie>
2023-08-24 12:06:56 +0100

    formatting: next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
 
 module.exports = {
   ...withNextra(),
@@ -12,62 +12,62 @@ module.exports = {
     return [
       // Runtime
       {
-        source: '/ecosystem/wasmer',
-        destination: '/runtime',
+        source: "/ecosystem/wasmer",
+        destination: "/runtime",
         permanent: true,
       },
       {
-        source: '/ecosystem/wasmer/getting-started',
-        destination: '/runtime/get-started',
+        source: "/ecosystem/wasmer/getting-started",
+        destination: "/runtime/get-started",
         permanent: true,
       },
       {
-        source: '/ecosystem/wasmer/usage',
-        destination: '/runtime/cli',
+        source: "/ecosystem/wasmer/usage",
+        destination: "/runtime/cli",
         permanent: true,
       },
       {
-        source: '/ecosystem/wasmer/wasmer-features',
-        destination: '/runtime/features',
+        source: "/ecosystem/wasmer/wasmer-features",
+        destination: "/runtime/features",
         permanent: true,
       },
       {
-        source: '/ecosystem/wasmer/building-from-source',
-        destination: '/developers/build-from-source',
+        source: "/ecosystem/wasmer/building-from-source",
+        destination: "/developers/build-from-source",
         permanent: true,
       },
       {
-        source: '/ecosystem/wasmer/building-from-source/testing',
-        destination: '/developers/testing',
+        source: "/ecosystem/wasmer/building-from-source/testing",
+        destination: "/developers/testing",
         permanent: true,
       },
 
       // Registry
       {
-        source: '/ecosystem/wapm',
-        destination: '/registry',
+        source: "/ecosystem/wapm",
+        destination: "/registry",
         permanent: true,
       },
       {
-        source: '/ecosystem/wapm/getting-started',
-        destination: '/registry/get-started',
+        source: "/ecosystem/wapm/getting-started",
+        destination: "/registry/get-started",
         permanent: true,
       },
       {
-        source: '/ecosystem/wapm/publishing-your-package',
-        destination: '/registry/get-started',
+        source: "/ecosystem/wapm/publishing-your-package",
+        destination: "/registry/get-started",
         permanent: true,
       },
       {
-        source: '/ecosystem/wapm/manifest',
-        destination: '/registry/manifest',
+        source: "/ecosystem/wapm/manifest",
+        destination: "/registry/manifest",
         permanent: true,
       },
       {
-        source: '/ecosystem/wapm/graphql-api',
-        destination: '/developers/graphql-api',
+        source: "/ecosystem/wapm/graphql-api",
+        destination: "/developers/graphql-api",
         permanent: true,
       },
     ];
   },
-}
+};
