3372920c6fb9e55ba6c3e1bfdeb1616c3afa5363
Christoph Herzog <chris@theduke.at>
2025-09-15 13:58:20 +0200

    SSH docs tweaks

--- components/CalloutProFeature.tsx
+++ components/CalloutProFeature.tsx
@@ -3,11 +3,11 @@ import { Callout } from "nextra-theme-docs";
 
 export default function CalloutProFeature() {
     return (
-     <Callout type="warning">
+     <Callout>
       This feature is only available for <b>Pro</b> and <b>Enterprise</b> plans.
 		      <br/>
 
-      <a href="https://wasmer.io/settings/billing" style={{ textDecoration: "underline" }} target="_blank">
+      <a href="https://wasmer.io/pricing" style={{ textDecoration: "underline" }} target="_blank">
         Upgrade your account now
       </a>!
     </Callout>
